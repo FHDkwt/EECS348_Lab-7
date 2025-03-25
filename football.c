ef345d46b95c369c6a6b81ae35b480e2d9e1f80f
FHDkwt <156952877+FHDkwt@users.noreply.github.com>
2025-03-25 16:20:50 +0500

    Update football.c
    Implement print_combinations and count_combinations in football.c.

--- football.c
+++ football.c
@@ -1,1 +1,41 @@
+#include <stdio.h>
 #include "football.h"
+
+void print_combinations(int points) {
+    int td_2pt, td_fg, td, fg, safety;
+    int count = 0; 
+    printf("Possible combinations of scoring plays if a team's score is %d:\n", points);
+    for (td_2pt = 0; td_2pt * 8 <= points; td_2pt++) {
+        for (td_fg = 0; td_fg * 7 <= points - td_2pt * 8; td_fg++) { 
+            for (td = 0; td * 6 <= points - td_2pt * 8 - td_fg * 7; td++) {
+                for (fg = 0; fg * 3 <= points - td_2pt * 8 - td_fg * 7 - td * 6; fg++) { 
+                    safety = (points - td_2pt * 8 - td_fg * 7 - td * 6 - fg * 3) / 2; 
+                    if (td_2pt * 8 + td_fg * 7 + td * 6 + fg * 3 + safety * 2 == points) {
+                        printf("%d TD + 2pt, %d TD + FG, %d TD, %d 3pt FG, %d Safety\n",
+                               td_2pt, td_fg, td, fg, safety);
+                        count++; 
+                    }
+                }
+            }
+        }
+    }
+}
+
+int count_combinations(int points) {
+    int td_2pt, td_fg, td, fg, safety;
+    int count = 0;
+    for (td_2pt = 0; td_2pt * 8 <= points; td_2pt++) {
+        for (td_fg = 0; td_fg * 7 <= points - td_2pt * 8; td_fg++) {
+            for (td = 0; td * 6 <= points - td_2pt * 8 - td_fg * 7; td++) {
+                for (fg = 0; fg * 3 <= points - td_2pt * 8 - td_fg * 7 - td * 6; fg++) {
+                    safety = (points - td_2pt * 8 - td_fg * 7 - td * 6 - fg * 3) / 2;
+                    if (td_2pt * 8 + td_fg * 7 + td * 6 + fg * 3 + safety * 2 == points) {
+                        count++;
+                    }
+                }
+            }
+        }
+    }
+
+    return count;
+}
