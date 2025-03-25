05b5174d4fe7cf3f44335ecbb61ec9ac1973afb5
FHDkwt <156952877+FHDkwt@users.noreply.github.com>
2025-03-25 15:53:01 +0500

    Update football_main.c
    Updated football_main.c with user input handling.

--- football_main.c
+++ football_main.c
@@ -1,5 +1,22 @@
+#include <stdio.h>
 #include "football.h"
 
-int main(int argc, char *argv[]) {
+int main() {
+    int score;  
+    printf("Enter 0 or 1 to STOP\n");
+    while (1) {
+        printf("Enter the NFL score: ");
+        scanf("%d", &score);
+        if (score <= 1) {
+            printf("Program terminated.\n");
+            break;
+        }
+        if (score < 2) {
+            printf("Invalid score! Score must be 2 or higher.\n");
+            continue;
+        }
+        find_combinations(score);
+    }
+
     return 0;
 }
